643945211980ad00434f901d142c0f9988ff55b0
Tom Dwyer <tom.dwyer@extropy.io>
2022-03-08 15:33:58 +0000

    Adding expSolTests

--- ExpertSolidity/contracts/DogCoin.sol
+++ ExpertSolidity/contracts/DogCoin.sol
@@ -1,9 +1,177 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
 
-contract DogCoin is ERC20 {
-    address[] public holders;
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
 
-    constructor() ERC20("DogCoin", "DOG") {}
+    function balanceOf(address account) external view returns (uint256);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+contract DogCoin is IERC20 {
+    using SafeMath for uint256;
+
+    string public constant name = "DogCoin";
+    string public constant symbol = "DOG";
+    uint8 public constant decimals = 18;
+
+    address[] holders;
+
+    mapping(address => uint256) balances;
+    mapping(address => uint256) holderLocation;
+    mapping(address => mapping(address => uint256)) allowed;
+
+    event HolderAdded(address indexed holder);
+    event HolderRemoved(address indexed holder);
+
+    uint256 totalSupply_;
+
+    constructor(uint256 total) {
+        totalSupply_ = total;
+        balances[msg.sender] = totalSupply_;
+        holders.push(msg.sender);
+        holderLocation[msg.sender] = holders.length;
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return totalSupply_;
+    }
+
+    function balanceOf(address tokenOwner)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        return balances[tokenOwner];
+    }
+
+    function transfer(address receiver, uint256 numTokens)
+        public
+        override
+        returns (bool)
+    {
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        balances[receiver] = balances[receiver].add(numTokens);
+        // Add to array
+        if (holderLocation[receiver] == 0) {
+            holders.push(receiver);
+            holderLocation[receiver] = holders.length;
+        }
+        // Remove from array if zero balance
+        if (balances[msg.sender] == 0) {
+            removeFromArray(msg.sender);
+        }
+        emit Transfer(msg.sender, receiver, numTokens);
+        return true;
+    }
+
+    function holdersLength() public view returns (uint256) {
+        return holders.length;
+    }
+
+    function holdersArrayLocation(address _holder)
+        public
+        view
+        returns (uint256)
+    {
+        return holderLocation[_holder];
+    }
+
+    function removeFromArray(address _holder) internal {
+        uint256 _holderIndex = holderLocation[_holder];
+        console.log("holderLocation[_holder]", _holderIndex);
+
+        console.log("holders[0]", holders[0]);
+
+        address _toShift = holders[_holderIndex - 1];
+        console.log("holders[_holderIndex - 1]", _toShift);
+
+        if (_holderIndex != 0) {
+            uint256 lastIndex = holders.length - 1;
+            console.log(lastIndex);
+            console.log(holders[lastIndex]);
+            if (lastIndex != _holderIndex) {
+                console.log("Going into loop");
+                // Update holderLocation mapping
+                holderLocation[_toShift] = _holderIndex;
+                // Update holders
+                holders[_holderIndex - 1] = holders[lastIndex];
+            }
+            holders.pop();
+        }
+        emit HolderRemoved(_holder);
+    }
+
+    function approve(address delegate, uint256 numTokens)
+        public
+        override
+        returns (bool)
+    {
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+
+    function allowance(address owner, address delegate)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        return allowed[owner][delegate];
+    }
+
+    function transferFrom(
+        address owner,
+        address buyer,
+        uint256 numTokens
+    ) public override returns (bool) {
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+
+        balances[owner] = balances[owner].sub(numTokens);
+        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
+        balances[buyer] = balances[buyer].add(numTokens);
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
+}
+
+library SafeMath {
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
 }
--- ExpertSolidity/test/dog-coin.test.js
+++ ExpertSolidity/test/dog-coin.test.js
@@ -1,17 +1,52 @@
-const { expect } = require("chai");
+const { expect } = require('chai');
 const { Contract } = require('ethers');
-const { ethers } = require("hardhat");
+const { ethers } = require('hardhat');
 
 let owner, exhOwner, exhOwner2, dogCoin;
 
-describe("DogCoin", async () => {
+const SUPPLY = 1000000000;
+
+describe('DogCoin', async () => {
   beforeEach(async () => {
-    [owner, exhOwner, exhOwner2] = await ethers.getSigners();
-    const DogCoin = await ethers.getContractFactory("DogCoin");
-    dogCoin = await Exhibitions.deploy();
+    [owner, user1, user2] = await ethers.getSigners();
+    const DogCoin = await ethers.getContractFactory('DogCoin');
+    dogCoin = await DogCoin.deploy(SUPPLY);
   });
 
-  it("Mint tokens and save owner to array", async () => {
+  it('Checks the name, symbol and token supply', async () => {
+    expect(await dogCoin.name()).to.equal('DogCoin');
+    expect(await dogCoin.symbol()).to.equal('DOG');
+    expect(await dogCoin.totalSupply()).to.equal(SUPPLY);
+  });
 
-  })
-})
+  it('Mint tokens and save owner to array', async () => {
+    const _startHolders = await dogCoin.holdersLength();
+    const _startBal = await dogCoin.balanceOf(user1.address);
+
+    await dogCoin.transfer(user1.address, 1);
+
+    const _newBal = await dogCoin.balanceOf(user1.address);
+    const _newHolders = await dogCoin.holdersLength();
+
+    expect(_startBal).to.equal(_newBal.sub(ethers.BigNumber.from(1)));
+    expect(_startHolders).to.equal(_newHolders.sub(ethers.BigNumber.from(1)));
+  });
+
+  it.only('Checks holder is removed from array when balance = 0', async () => {
+    await dogCoin.transfer(user1.address, 1);
+
+    const _location = await dogCoin.holdersArrayLocation(user1.address);
+    console.log('_location:', _location);
+
+    const _startBalance = await dogCoin.balanceOf(user1.address);
+    console.log('_startBalance:', _startBalance);
+
+    await dogCoin.connect(user1).transfer(owner.address, _startBalance);
+
+    const _newBalance = await dogCoin.balanceOf(user1.address);
+    console.log('_newBalance:', _newBalance);
+
+    const _location2 = await dogCoin.holdersArrayLocation(user1.address);
+    console.log('_location2:', _location2);
+  });
+});
