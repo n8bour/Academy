26931e514705e17e8c434064649023f5b818368b
laurence <laurence@extropy.io>
2022-05-10 09:11:55 +0100

    restore

--- DeFiHardhat/hardhat.config.js
+++ DeFiHardhat/hardhat.config.js
@@ -13,24 +13,27 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-// To console.log() in contract use in the contract: import "hardhat/console.sol";
-
-// To test: npx hardhat test
-
 // module.exports = {
-//   defaultNetwork: "ganache",
+//   defaultNetwork: 'ganache',
 //   networks: {
 //     ganache: {
-//     // change URL to the URL from your Ganache
-//       url: "http://127.0.0.1:7545",
+//       // change URL to the URL from your Ganache. See README if the port is not 8545.
+//       url: 'http://127.0.0.1:8545',
 //       gasLimit: 6000000000,
 //       defaultBalanceEther: 1000,
 //     },
 //   },
-//   solidity: "0.8.0",
+//   solidity: '0.8.0',
 // };
 
-
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-   solidity: "0.8.8",
+  solidity: "0.8.4",
+  networks: {
+    fork: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
 };
